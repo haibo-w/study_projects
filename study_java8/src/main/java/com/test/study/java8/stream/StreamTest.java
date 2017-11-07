17fb4b015d58d42770ab11185e7a2ed3f2b03db5
r8Kc0de <haibo.w@live.com>
2017-11-07 17:33:14 +0800

    stream

--- study_java8/src/main/java/com/test/study/java8/stream/StreamTest.java
+++ study_java8/src/main/java/com/test/study/java8/stream/StreamTest.java
@@ -20,13 +20,17 @@ public class StreamTest {
 	};
 
 	static class Beauty {
-		private final Status status;
+		private Status status = Status.CLOSED;
 
 		private String name;
 
 		private Integer size;
 
 		private String nation;
+		
+		public Beauty() {
+			
+		}
 
 		public Beauty(Status status, String name, Integer size, String nation) {
 			super();
@@ -67,7 +71,9 @@ public class StreamTest {
 				new Beauty(Status.CLOSED, "CLS", 37, "JAPAN"),
 				new Beauty(Status.OPEN, "JINGTIAN", 32, "CHINA"),
 				new Beauty(Status.OPEN, "XZMLY", 34, "JAPAN"),
-				new Beauty(Status.OPEN, "BINGBING", 33, "CHINA")
+				new Beauty(Status.OPEN, "JACK", 34, "JAPAN"),
+				new Beauty(Status.OPEN, "BINGBING", 33, "CHINA"),
+				new Beauty(Status.OPEN, "XIXI", 36, "CHINA")
 		);
 		
 //		List<Integer> collect = new Random().ints(30, 37).limit(500000).boxed().collect(Collectors.toList());
@@ -88,7 +94,41 @@ public class StreamTest {
 				max().
 				getAsInt();
 		System.out.println(" open max size:"+asInt);
-
+	}
+	
+	@Test
+	public void testCount() {
+		System.out.println("stream count:"+beauties.stream().count());
+		
+		beauties.stream().sorted((s1,s2)->{
+			if(s1.size > s2.size) {
+				return 1;
+			}else if(s1.size < s2.size) {
+				return -1;
+			}else {
+				return 0;
+			}
+		}).forEach(s -> System.out.println(s.name + " " +s.size));
+		
+		
+		// map 使用
+		System.out.print("\nMap distince collect collectors ");
+		beauties.stream().map(s -> s.size).distinct().collect(Collectors.toList()).forEach( s -> System.out.print(s+"\t"));
+		
+	}
+	
+	@Test
+	public void testReduce() {
+		List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		System.out.println("ints sum is:" + ints.stream().reduce(0, (sum, item) -> sum + item));
+		
+		System.out.println("ints sum is:" + ints.stream().reduce((sum, item) -> sum + item).get());
+		
+		beauties.stream().collect(Collectors.groupingBy(Beauty::getNation,Collectors.toList())).forEach((e , gplist) ->{
+			double d = gplist.stream().mapToInt(Beauty::getSize).average().getAsDouble();
+			System.out.println(e + " count:" + gplist.size() + " avg:" + d);
+		});
+		
 	}
 
 	@Test
